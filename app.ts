29d85ff9492b20a69db7f7e6958d7b4a950cfccf
Jake Declock <derilojake2528@gmail.com>
2026-05-17 10:29:46 +0800

    fix vercel

--- app.ts
+++ app.ts
@@ -6,17 +6,21 @@ import notificationRoutes from "./modules/routes/notification.routes";
 import cors from "cors";
 
 const app = express();
+
 app.use(express.json());
+
+const allowedOrigins = process.env.FRONTEND_URL || "http://localhost:5173";
+
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: allowedOrigins,
     credentials: true,
   }),
 );
-//roustes
 
 app.use("/api", authRoutes);
 app.use("/api/clients", clientRoutes);
 app.use("/api/transactions", transactionRoutes);
 app.use("/api/notifications", notificationRoutes);
+
 export default app;
--- server.ts
+++ server.ts
@@ -4,19 +4,6 @@ dotenv.config();
 import app from "./app";
 import { connectDB } from "./config/db";
 
-const PORT = process.env.PORT || 5000;
+connectDB();
 
-const startServer = async () => {
-  try {
-    await connectDB();
-
-    app.listen(PORT, () => {
-      console.log(`Server running on port ${PORT}`);
-    });
-  } catch (error) {
-    console.error("Failed to start server:", error);
-    process.exit(1);
-  }
-};
-
-startServer();
+export default app;
